27f982113c90dfda9a6e8a1e9173238458f70e24
Max Countryman <maxc@me.com>
2015-07-20 15:51:16 -0700

    python-bcrypt -> py-bcrypt

--- flask_bcrypt.py
+++ flask_bcrypt.py
@@ -11,7 +11,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-__version_info__ = ('0', '6', '2')
+__version_info__ = ('0', '6', '3')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Max Countryman'
 __license__ = 'BSD'
